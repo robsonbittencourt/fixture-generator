3ed779803dc75c03c128824aa120c1c5955f8867
robsonbittencourt <robson.luizv@gmail.com>
2017-07-01 14:07:43 -0300

    Resolve Sonar issue
    Resources should be closed
    https://sonarcloud.io/organizations/robsonbittencourt-github/rules#rule_key=squid%3AS2095

--- src/main/java/com/utility/generator/file/FileBuilder.java
+++ src/main/java/com/utility/generator/file/FileBuilder.java
@@ -10,13 +10,11 @@ import com.utility.generator.exception.FixtureGeneratorException;
 public class FileBuilder {
 
 	public void createFile(String className, String path, String content) {
-		try {
-			File file = new File(path + "/" + className + ".java");
-			file.getParentFile().mkdirs();
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
+		File file = new File(path + "/" + className + ".java");
+		file.getParentFile().mkdirs();
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()))){
 			bw.write(content);
-			bw.close();
 		} catch (IOException e) {
 			throw new FixtureGeneratorException("An error occurred while generating the file: " + e.getMessage());
 		}
